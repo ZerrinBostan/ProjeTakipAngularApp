251ec50c5e4557c42f7d4bf03561cf41dabff1d2
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2019-04-14 14:43:04 +0300

    ProfileServices added.

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Students } from '../login/students.model';
+import { environment } from 'src/environments/environment';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ImageService {
-  constructor() { }
+export class ProfileService {
+  constructor(private http: HttpClient, private localStorage: LocalStorageService) { }
+  getUserInfo(): Observable<Students> {
+    const id = this.localStorage.get('_id');
+    const url = `${environment.apiUrl}students/${id}`;
+    return this.http.get<Students>(url);
+  }
+
 }
