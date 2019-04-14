17667264a31a983c7fc2079849546c8ecbc9edff
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2019-04-14 12:16:56 +0300

    Login form business

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthenticateService } from './authenticate.service';
+import { Students } from './students.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   userName: string;
   password: string;
-  constructor() { }
+  registerForm = this.formBuilder.group({
+    identityNumber: ['', Validators.required],
+    email: ['', Validators.email],
+    name: ['', Validators.required],
+    surname: ['', Validators.required],
+    phoneNumber: ['', Validators.required],
+    studentNumber: ['', Validators.required],
+    studentClass: [''],
+    studentClassGroup: [''],
+    password: ['', Validators.required],
+    repassword: ['', Validators.required]
+  });
+  loginForm = this.formBuilder.group({
+    studentNo: ['', Validators.required],
+    studentPassword: ['', Validators.required]
+  });
+  student: Students;
+  constructor(public formBuilder: FormBuilder,private authenticateService: AuthenticateService, private router: Router) { }
 
   ngOnInit() {
   }
+  onRegister() {
+    this.student = this.registerForm.value;
+    this.authenticateService.register(this.student).subscribe((data)  => {
+        console.log(data);
+    });
+  }
+  onLogin() {
+    this.userName = this.loginForm.get('studentNo').value;
+    this.password = this.loginForm.get('studentPassword').value;
+    this.authenticateService.authenticate(this.userName, this.password).subscribe((data) => {
+      this.authenticateService.isLogin.next(data);
+      this.router.navigate(['']);
+    });
+    }
 
 }
