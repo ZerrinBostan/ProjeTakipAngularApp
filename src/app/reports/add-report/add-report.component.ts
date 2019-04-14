7e15c7d77c67a90aa7819a0ba9eba7ae2d1f7b17
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2019-04-14 12:17:53 +0300

    Delete report component added but not working right now.

---
+++ src/app/reports/add-report/add-report.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TdTextEditorComponent } from '@covalent/text-editor';
+import { MatDialogRef } from '@angular/material';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ReportsService } from '../reports.service';
+import { Reports } from '../reports.model';
+
+@Component({
+  selector: 'app-add-report',
+  templateUrl: './add-report.component.html',
+  styleUrls: ['./add-report.component.css']
+})
+export class AddReportComponent implements OnInit {
+  @ViewChild('textEditor') textEditor: TdTextEditorComponent;
+  reportForm = this.formBuilder.group({
+    no: ['', Validators.required],
+    name: '',
+    surname: '',
+    revisionNumber: '',
+    date: '',
+    description: ''
+  });
+  report: Reports;
+  constructor(public dialogRef: MatDialogRef<AddReportComponent>, private formBuilder: FormBuilder, private reportService: ReportsService) { }
+  ngOnInit() {
+    this.textEditor.options.lineWrapping = true;
+  }
+  onAdd() {
+    this.report = this.reportForm.value;
+    this.reportService.addReport(this.report).subscribe((data) => {
+      console.log(data);
+    });
+    this.dialogRef.close();
+  }
+
+}
